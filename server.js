4a165a968da51f9200f8871d7498d2caa21f3737
Bruno-ui374 <bruartin256@gmail.com>
2026-03-22 11:02:41 +0300

    Contacts Part 1

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const app = express();
+const mongodb = require('./data/database');
+
 const PORT = process.env.PORT || 3000;
 
 app.use('/', require('./routes'));
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+
+mongodb.initDb((err, mongodb) => {
+    if (err) {
+        console.log(err);
+    } else {
+        app.listen(PORT, () => {
+            console.log(`Database is listening and node is running on port ${PORT}`);
+        });
+    }
 });
 
